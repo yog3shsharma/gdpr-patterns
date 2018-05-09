cbf4d4bb506c36043b7fa55012ac5fdb31d18e79
Dinis Cruz <dinis.cruz@owasp.org>
2018-05-09 09:39:43 +0100

    Added Hugo config file

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -5,7 +5,10 @@ echo "*****************************"
 cd bin/Docker
 ls
 
-#docker build -t owasp/gdpr-patterns . || exit 1
-docker build -t owasp/gdpr-patterns .
+docker build -t diniscruz/gdpr-patterns . || exit 1
+#docker build -t diniscruz/gdpr-patterns .
 
-docker run --rm -it owasp/gdpr-patterns bash
+#docker run --rm -it -p 3001:3000 diniscruz/gdpr-patterns bash
+#docker run --rm -it -p 3000:3000 diniscruz/gdpr-patterns
+
+#docker push diniscruz/gdpr-patterns
