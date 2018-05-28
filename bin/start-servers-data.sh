4a227a3a0a23454b7f7fea4247a711a1d73ad9aa
Soti Sima <soti.sima@UK03961.local>
2018-05-28 01:19:55 +0100

    update

--- bin/start-servers-data.sh
+++ bin/start-servers-data.sh
@@ -2,10 +2,6 @@ echo "**************************************************"
 echo "***   Starting Hugo Server in Dev mode - DATA  ***"
 echo "**************************************************"
 
-cd gdpr-patterns-presentation
-hugo serve  &
-cd ..
-
 node_modules/.bin/nodemon \
     -w  ./node/jira-proxy/src/  \
     -w ./node/jira-issues/src/  \
