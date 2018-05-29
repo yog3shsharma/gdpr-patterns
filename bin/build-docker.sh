1e32770d2d066515088cd7c3477ee4af4afafa44
Soti Sima <soti.sima@UK03961.local>
2018-05-29 21:49:09 +0100

    update

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build
-docker build -t gdpr-patterns:latest bin/docker_all/ ;
+docker build -t gdpr-patterns:latest bin/docker_all/ --no-cache;
 
 # Tag
 docker tag gdpr-patterns pbxgs/gdpr-patterns ;
