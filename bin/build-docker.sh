99a9620279a3629299afcbd60ef8809916c82ad4
Soti Sima <soti.sima@UK03961.local>
2018-05-29 21:14:40 +0100

    update

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build
-docker build -t gdpr-patterns:latest bin/docker_all/ --no-cache;
+docker build -t gdpr-patterns:latest bin/docker_all/ ;
 
 # Tag
 docker tag gdpr-patterns pbxgs/gdpr-patterns ;
--- utils/setup_end_points.py
+++ utils/setup_end_points.py
@@ -9,82 +9,104 @@ server="localhost"
 r = requests.get("http://" + server + ":3000/api/neo4j/delete/all?pretty")
 print "0,", r.status_code
 
+'''
 # REMOVE JIRA
-r = requests.delete("http://" + server + ":3000/api/admin/datafolder")
-print "0,", r.status_code
+#r = requests.delete("http://" + server + ":3000/api/admin/datafolder")
+#print "0,", r.status_code
+#time.sleep(3)
 
-#exit(0)
+
+requestMade = False
 
 while True:
     r = requests.get("http://" + server + ":3000/api/admin/countfiles")
-    requestMaded=False
     if r.status_code==200:
         data = json.loads(r.content)
         if data['files']>8501:
             print 'JIRA tickets: ' + str(data['files'])
             break
-        elif data['files']<=10:
-            if requestMaded==False:
-                r = requests.get( "http://" + server + ":3000/api/jira-server/all_issues?jql=project=GDPR%20%20or%20project%20=%20TM%20OR%20project%20=%20SEC%20or%20project%20=%20RISK%20or%20project%20=%20VULN")
-                print r.status_code
-                requestMaded=True
-            time.sleep(3)
         else:
+            if requestMade==False:
+                r = requests.get( "http://" + server + ":3000/api/jira-server/all_issues?jql=project=GDPR%20%20or%20project%20=%20TM%20OR%20project%20=%20SEC%20or%20project%20=%20RISK%20or%20project%20=%20VULN")
+                print "Request all JIRA tickets: HTTP CODE: ", r.status_code
+                requestMade=True
             print 'Waiting JIRA download: ' + str(data['files'])
             time.sleep(3)
+
+exit(0)
 
 
 r = requests.get("http://" + server + ":3000/api/jira-server/setup?pretty")
 print "1,", r.status_code
+time.sleep(3)
 
 
 r = requests.get("http://" + server + ":3000/api/jira-server/issue/RISK-2?pretty")
 print "2,", r.status_code
+time.sleep(3)
 
 r = requests.get("http://" + server + ":3000/api/jira-server/track-queries/create/open-projects?jql=project=SEC%20and%20issuetype%20=Project%20and%20status%20=%20Open")
 print "3,", r.status_code
+time.sleep(3)
 
 r = requests.get("http://" + server + ":3000/api/jira-server/track-queries/update/open-projects")
 print "4,", r.status_code
+time.sleep(3)
 
 r = requests.get("http://" + server + ":3000/api/jira-server/mappings/issues/files?pretty")
 print "5,", r.status_code
+time.sleep(3)
 
 r = requests.get("http://" + server + ":3000/api/jira-server/mappings/create?pretty")
 print "6,", r.status_code
+time.sleep(3)
 
 r = requests.get("http://" + server + ":3000/api/jira/fields/schema?pretty")
+print "7,", r.status_code
+time.sleep(3)
+
+'''
+
+r = requests.get("http://" + server + ":3000/api/admin/git/clone",  timeout=60)
+print "6,", r.status_code
+
+r = requests.get("http://" + server + ":3000/api/admin/git/pull",  timeout=60)
 print "7,", r.status_code
 
 for x in range(1, 2):
     print "Round: ", x
     try:
-        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/VULN-?pretty", timeout=30)
+        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/VULN-?pretty", timeout=60)
         print "8,", r.status_code
+        time.sleep(3)
     except:
         print "8, "
 
     try:
-        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/SEC-?pretty", timeout=30)
+        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/SEC-?pretty", timeout=60)
         print "9,", r.status_code
+        time.sleep(3)
     except:
         print "9, "
 
     try:
-        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/GDPR-?pretty", timeout=30)
+        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/GDPR-?pretty", timeout=60)
         print "10,", r.status_code
+        time.sleep(3)
     except:
         print "10, "
 
     try:
-        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/RISK-?pretty", timeout=30)
+        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/RISK-?pretty", timeout=60)
         print "11,", r.status_code
+        time.sleep(3)
     except:
         print "11, "
 
     try:
-        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/TM-?pretty", timeout=30)
+        r = requests.get("http://" + server + ":3000/api/neo4j/nodes/create-regex/TM-?pretty", timeout=60)
         print "12,", r.status_code
+        time.sleep(3)
     except:
         print "12, "
 
