c71aeb928794072b6d1a8f85fdf53565806bb1e8
Soti Sima <soti.sima@UK03961.local>
2018-06-01 20:33:03 +0100

    docker build script to include data script

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -2,9 +2,12 @@
 
 # Build
 docker build -t gdpr-patterns:latest bin/docker_all/ --no-cache;
+docker build -t gdpr-patterns-data:latest bin/docker/gdpr-patterns-data --no-cache;
 
 # Tag
 docker tag gdpr-patterns pbxgs/gdpr-patterns ;
+docker tag gdpr-patterns-data pbxgs/gdpr-patterns-data ;
 
 # Push
 docker push pbxgs/gdpr-patterns ;
+docker push pbxgs/gdpr-patterns-data ;
